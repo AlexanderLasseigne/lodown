8c6c3ddc8165af02c99f804e3a4f9b7b70654866
Alexander Lasseigne III <alexanderlasseigne3rd@gmail.com>
2022-08-20 19:35:46 +0000

    did some lodown up to last func

--- index.js
+++ index.js
@@ -30,8 +30,73 @@ module.exports.each = each;
  * @param {Any value} value: Function takes in any input value.
  * @returns {Any value} value: Function
  */
-
 function identity(value){
     return value;
 }
 module.exports.identity = identity;
+
+/**
+ * typeOf: takes in any value and returns the type
+ * 
+ * @param {Any value} value: Takes in any input value.
+ * @returns {A string} :type of datatype returned as a 
+ */
+function typeOf(value){ 
+    if (typeof value === 'string'){
+        return 'string';
+    } if (value === true || value === false){
+        return 'boolean';
+    } if (Array.isArray(value)){        
+        return 'array';
+    } if (value === undefined){
+        return 'undefined';      
+    } if (value === null){
+        return 'null';
+    } if (!isNaN(value)){
+        return 'number'
+    } if (typeof value === 'function'){
+        return 'function';
+    } if (typeof value === 'object'){
+        return 'object';
+    }
+} 
+module.exports.typeOf = typeOf;
+
+/**
+ * first: takes in array and number and verifies if 'arr' is an ARRAY.
+ * IF NOT- an empty array is returned.
+ * Then it tests IF 'num' is given.
+ * IF NOT- return first element in array.
+ * ELSE- return the first number of elements in 'arr' specified by 'num'
+ */
+function first(arr, num){ 
+    //IF array is not an array return []
+    if (!Array.isArray(arr)){ 
+        return [];
+    } else if (!num){ //if no num given as argument return first element
+        return arr[0];
+    } 
+    return arr.splice(0, num); // else return first num of elements
+}
+module.exports.first = first;
+
+/**
+ * last: tests IF 'arr' is not an array, return []
+ * IF 'num' is not given or not a number, return just the last element in <array>.
+ * Otherwise, return the last <number> items of <array>
+ */
+ function last(arr, num){
+    //test for neg num &&
+    //IF array is not an array return []
+    if (!Array.isArray(arr) || num <= 0){
+        return [];
+    } if (num > arr.length){
+        return arr;
+    }
+      if (!num || num > arr.length){
+        return arr[arr.length - 1];
+    } if (num >= 0){
+    return arr.slice(-num);
+    }
+}
+module.exports.last = last;
